9af3e69a2bf744bfebbf1ae8a05a5561dd72d096
Glitch (delightful-diagnostic) <none>
2019-11-28 07:11:20 +0000

    🛍🚲 Checkpoint ./server.js:6079345/38

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ mongo.connect(process.env.DATABASE, (err, db) => {
      passport.use(new GitHubStrategy({
           clientID: process.env.GITHUB_CLIENT_ID,
           clientSecret: process.env.GITHUB_CLIENT_SECRET,
-          callbackURL: 'https://delightful-diagnostic.glitch.me/auth/github/callback'
+          callbackURL: "https://delightful-diagnostic.glitch.me/auth/github/callback"
      },
         
       function(accessToken, refreshToken, profile, cb) {
